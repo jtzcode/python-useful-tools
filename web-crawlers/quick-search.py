690d39987a529a2350e97fd14d5a951b678cb02a
Tianze Jiang <tianze.jiang@citrix.com>
2020-12-10 16:27:10 +0800

    open google results

--- web-crawlers/quick-search.py
+++ web-crawlers/quick-search.py
@@ -1,7 +1,15 @@
 import sys, webbrowser, bs4, requests
 
-print('Googling results for you...')
-
-res = requests.get('http://google.com/search?q=' + ' '.join(sys.argv[1:]))
+res = requests.get('https://www.google.com/search?q=' + ' '.join(sys.argv[1:]))
 res.raise_for_status()
+#print(res.text)
 
+print('Googling results for you...Keywords: ' + ' '.join(sys.argv[1:]))
+soup = bs4.BeautifulSoup(res.text, features='html.parser')
+linkElements = soup.select('a')
+
+numToOpen = min(5, len(linkElements))
+
+print("Will open %s pages as results" % numToOpen)
+for i in range(numToOpen):
+    webbrowser.open('https://google.com' + linkElements[i].get('href'))
